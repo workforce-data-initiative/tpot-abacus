b13c4ad48f5c854a52b7f77f403b447fbfa61a11
Ndagi Stanley <ndagis@gmail.com>
2017-07-11 15:49:32 +0300

    [FInishes #146458459] Update notebook and master module

--- abacus_tpot/__init__.py
+++ abacus_tpot/__init__.py
@@ -1,2 +1,4 @@
+from abacus_tpot import master
+
 def help():
     return "Abacus is ready to go ..."
--- abacus_tpot/master.py
+++ abacus_tpot/master.py
@@ -1,10 +1,11 @@
-# To output master tables based on the transactioanl tables input
+# To output master tables based on the transactional tables input
 
 
 def participant_master_record_create():
     """
     Reads the participant master records table to create the record Table.
     """
+    print("participant_master_record_create")
     pass
 
 
@@ -12,11 +13,15 @@ def program_outcomes():
     """
     Read participant master records table to give program outcome table.
     """
+    print("program_outcomes")
     pass
 
 
 def program_outcomes_table_anonymization_check():
     """
-    Ensure that sufficient data is averaged to ensure anonymity.
+    Ensure that sufficient data is averaged to ensure anonymity plus
+    that the encryption or removal of personally identifiable
+    information has been done on the data sets.
     """
+    print("program_outcomes_table_anonymization_check")
     pass

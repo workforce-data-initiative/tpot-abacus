71835c3067384264984bd37e619765e2995c1a64
Tom Plagge <tplagge@gmail.com>
2018-02-21 15:59:14 -0600

    Add toy implementation of median wage

--- abacus_tpot/master.py
+++ abacus_tpot/master.py
@@ -1,27 +1,55 @@
-# To output master tables based on the transactional tables input
+#!/usr/bin/env python
+import psycopg2
+import pandas as pd
+import numpy as np
+
+ANONYMIZATION_THRESHOLD = 10
+WAREHOUSE_URI = 'postgres://localhost'
+conn = psycopg2.connect(WAREHOUSE_URI)
+cached_participant_record = None
+
+def get_participant_master_record():
+    global cached_participant_record
+    if cached_participant_record is not None:
+        return cached_participant_record
+
+    df = pd.read_sql('''
+        SELECT p.ParticipantID, p.ProgramID, p.ExitTypeID, p.ExitDate, p.Enrolled,
+               w.WageAmt, w.WageStartDate, w.WageEndDate,
+               pa.WioaParticipant, pa.WioaItaParticipant
+            FROM Wage w
+                LEFT JOIN ParticipantProgram p ON w.ParticipantID=p.ParticipantID
+                LEFT JOIN Participant pa ON w.ParticipantID=pa.ParticipantID
+    ''', conn)
+
+    cached_participant_record = df
+    return df
 
 
-def participant_master_record_create():
-    """
-    Reads the participant master records table to create the record Table.
-    """
-    print("participant_master_record_create")
-    pass
+def program_participants(program_id):
+    df = get_participant_master_record()
+    return df[df.programid==program_id].participantid.unique()
 
 
-def program_outcomes():
-    """
-    Read participant master records table to give program outcome table.
-    """
-    print("program_outcomes")
-    pass
+def median_wage(participants, reporting_period_start, reporting_period_end):
+    if len(set(participants)) < ANONYMIZATION_THRESHOLD:
+        return None
+
+    reporting_period_start = pd.to_datetime(reporting_period_start)
+    reporting_period_end = pd.to_datetime(reporting_period_end)
+    df = get_participant_master_record()
+    participant_wages = df[df.participantid.isin(participants)]
+    monthly_wages = pd.DataFrame(
+        [[participantid, wageamt, d] for
+        participantid, programid, exittypeid, exitdate, enrolled,
+        wageamt, wagestartdate, wageenddate, wioaparticipant, wioaitaparticipant
+        in df.itertuples(index=False)
+        for d in pd.date_range(wagestartdate, wageenddate, freq='MS')],
+        columns=['pid', 'amt', 'date'])
+    monthly_wages = monthly_wages[monthly_wages.date >= reporting_period_start]
+    monthly_wages = monthly_wages[monthly_wages.date <= reporting_period_end]
+    wages_by_person = monthly_wages.groupby('pid').amt.sum()
+    return np.median(wages_by_person.values)
 
 
-def program_outcomes_table_anonymization_check():
-    """
-    Ensure that sufficient data is averaged to ensure anonymity plus
-    that the encryption or removal of personally identifiable
-    information has been done on the data sets.
-    """
-    print("program_outcomes_table_anonymization_check")
-    pass
+print(median_wage(program_participants(123456), '1/1/2017', '12/31/2017'))

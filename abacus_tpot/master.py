5bd259b0bd544b8a8600d857dc3fb4a5a6a99fb4
Tom Plagge <tplagge@gmail.com>
2018-02-21 16:02:15 -0600

    Add some comments

--- abacus_tpot/master.py
+++ abacus_tpot/master.py
@@ -9,6 +9,9 @@ conn = psycopg2.connect(WAREHOUSE_URI)
 cached_participant_record = None
 
 def get_participant_master_record():
+    """
+    Return a table of wage data based on the transactional DB
+    """
     global cached_participant_record
     if cached_participant_record is not None:
         return cached_participant_record
@@ -27,11 +30,19 @@ def get_participant_master_record():
 
 
 def program_participants(program_id):
+    """
+    Return a list of participant IDs in a given program ID
+    """
     df = get_participant_master_record()
     return df[df.programid==program_id].participantid.unique()
 
 
 def median_wage(participants, reporting_period_start, reporting_period_end):
+    """
+    Return the median total wage over a reporting period for a list of participants
+    """
+
+    # Check anonymization cutoff
     if len(set(participants)) < ANONYMIZATION_THRESHOLD:
         return None
 
@@ -39,6 +50,8 @@ def median_wage(participants, reporting_period_start, reporting_period_end):
     reporting_period_end = pd.to_datetime(reporting_period_end)
     df = get_participant_master_record()
     participant_wages = df[df.participantid.isin(participants)]
+
+    # Create a table with one row per month, per participant, per wage record.
     monthly_wages = pd.DataFrame(
         [[participantid, wageamt, d] for
         participantid, programid, exittypeid, exitdate, enrolled,
@@ -46,9 +59,14 @@ def median_wage(participants, reporting_period_start, reporting_period_end):
         in df.itertuples(index=False)
         for d in pd.date_range(wagestartdate, wageenddate, freq='MS')],
         columns=['pid', 'amt', 'date'])
+    # Limit to the reporting period, including partial months.
     monthly_wages = monthly_wages[monthly_wages.date >= reporting_period_start]
     monthly_wages = monthly_wages[monthly_wages.date <= reporting_period_end]
+
+    # Compute total wages for each participant
     wages_by_person = monthly_wages.groupby('pid').amt.sum()
+
+    # Return the median
     return np.median(wages_by_person.values)
 
 

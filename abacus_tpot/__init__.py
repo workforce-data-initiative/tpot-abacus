480cba1f5cb8f37617d71b386f0bbbed5d4dacf9
NdagiStanley <stanley@brighthive.io>
2018-06-06 23:24:52 +0300

    Fix tests

--- abacus_tpot/__init__.py
+++ abacus_tpot/__init__.py
@@ -1,2 +1,1 @@
-from . import input_db
 from . import outcomes
--- abacus_tpot/tests/test_master.py
+++ abacus_tpot/tests/test_master.py
@@ -6,50 +6,6 @@ import numpy as np
 
 from abacus_tpot.outcomes.wioa import median_wage_n_quarters_after_exit,\
     employed_n_quarters_after_exit
-from abacus_tpot.input_db.participants import filter_participants
-
-
-class TestParticipantList(TestCase):
-    def setUp(self):
-        self.participant_table = pd.DataFrame(
-            {
-                'participant_id': range(1, 21),
-                'provider_id':    np.append(np.ones(10), np.ones(10) * 2),
-                'program_code':   np.append(np.ones(10), np.ones(10) * 2),
-                'exit_type':      np.append(np.ones(16), np.zeros(4)),
-                'entry_date':     np.append(np.repeat(pd.to_datetime('1/1/2016'), 17),
-                                            np.repeat(pd.to_datetime('7/1/2016'), 3)),
-                'exit_date':      np.append(np.repeat(pd.to_datetime('6/30/2016'), 18),
-                                            np.repeat(pd.to_datetime('12/31/2016'), 2))
-            })
-
-    def test_programid_filter(self):
-        s = filter_participants(self.participant_table, program_code=1)
-        self.assertEqual(len(s), 10)
-
-    def test_exittype_filter(self):
-        s = filter_participants(self.participant_table, exit_type=1)
-        self.assertEqual(len(s), 16)
-
-    def test_min_entry_date_filter(self):
-        s = filter_participants(self.participant_table,
-                                min_entry_date='6/1/2016')
-        self.assertEqual(len(s), 3)
-
-    def test_min_exit_date_filter(self):
-        s = filter_participants(self.participant_table,
-                                min_exit_date='7/1/2016')
-        self.assertEqual(len(s), 2)
-
-    def test_max_entry_date_filter(self):
-        s = filter_participants(self.participant_table,
-                                max_entry_date='6/1/2016')
-        self.assertEqual(len(s), 17)
-
-    def test_max_exit_date_filter(self):
-        s = filter_participants(self.participant_table,
-                                max_exit_date='7/1/2016')
-        self.assertEqual(len(s), 18)
 
 
 class TestMedianWage(TestCase):
